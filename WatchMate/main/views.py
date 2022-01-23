fd94a27d7fb55ee719994225e7114c103a6ff2e3
kunal <kunalbhatia868@gmail.com>
2022-01-23 20:36:52 +0530

    views created using JsonResponse

--- WatchMate/main/views.py
+++ WatchMate/main/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Movie
 # Create your views here.
+
+def movie_list(request):
+    movies=Movie.objects.all()
+    data={
+        'movies':list(movies.values())
+    }
+    return JsonResponse(data)
+
+def movie_detail(request,pk):
+    movie=get_object_or_404(Movie,id=pk)
+    data={
+        'name':movie.name,
+        'description':movie.description,
+        'active':movie.active
+    }
+    return JsonResponse(data)
+        
