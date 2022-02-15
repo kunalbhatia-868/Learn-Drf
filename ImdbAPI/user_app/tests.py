a53802a90224743b9bfa0dfec1847b17df858f9e
kunal <kunalbhatia868@gmail.com>
2022-02-15 16:11:01 +0530

    Added some test cases for User register and login

--- ImdbAPI/user_app/tests.py
+++ ImdbAPI/user_app/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class RegisterTestCase(APITestCase):
+    
+    def test_register(self):
+        data={
+            "username":"testcase",
+            "email":"testcase@gmai.com",
+            "password":"testcasehere",
+            "password2":"testcasehere"
+        }
+        response=self.client.post(reverse('register'),data=data)
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        
+class LoginLogoutTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user=User.objects.create_user(username="example",password="password123")
+    
+    def test_login(self):
+        data={
+            "username":"example",
+            "password":'password123'
+        }
+        response=self.client.post(reverse('login'),data=data)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+    
+    # def test_logout(self):
+    #     # data={
+    #     #     "username":"example",
+    #     #     "password":'password123'
+    #     # }
+    #     # response=self.client.post(reverse('login'),data=data)
+    #     # token=response.data['token']
+    #     # print(token)
+    #     # self.client.credentials(HTTP_AUTHORIZATION='BEARER '+token)
+    #     # logout_response=self.client.post(reverse('logout'))
+    #     # self.assertEqual(logout_response.status_code,status.HTTP_200_OK)
+    #     self.token=Token.objects.get(user__username="example")
+    #     self.client.credentials(HTTP_AUTHORIZATION='Token '+self.token.key)
+    #     response=self.client.post(reverse('logout'))
+    #     self.assertEqual(response.status_code,status.HTTP_200_OK)
--- ImdbAPI/user_app/urls.py
+++ ImdbAPI/user_app/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns=[
     path("login/",obtain_auth_token,name="login"),
     path("logout/",logout_view,name="logout"),
-    path("register/",registration_view,name="regster"),
+    path("register/",registration_view,name="register"),
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
